98d04c82a16508eae911fb3188c8d300afaf13a3
MojiTom <tom.hoyt@zoho.com>
2021-04-27 14:35:29 -0500

    Testing our Github workflow and adding useful comments.

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,6 +1,11 @@
 import React, { Component }  from 'react';
 import { NavLink } from 'react-router-dom';
 
+// "Menu" is apparently a keyword in React, so I named the componenent Choices. - TH
+
+// The following component is just a placeholder, but it keeps everything working until
+// it's replaced by functional code. - TH
+
 class Choices extends Component {
 
     render () { 
